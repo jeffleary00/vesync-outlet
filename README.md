dfe3eda6a1f49ae90553bbb3ce3a67c3c266a1c7
Jeffrey Leary <jeffreyleary@Jeffreys-MacBook-Air.local>
2019-01-16 09:51:45 -0500

    added setup.py

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # vesync-outlet
 Python module to control Vesync WiFi power outlets
+
+## Installation
+```
+pip3 install vesync-outlet
+```
+
+## Usage
+```
+from vesync-outlet import Vesync
+
+hashpw = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+vesync = Vesync(username, hashpw)
+devices, response = vesync.get_outlets()
+for d in devices:
+  data, response = vesync.turn_on(d['id'])
+  data, response = vesync.turn_off(d['id'])
+```
+
+## Methods
+The methods below return a tuple. The first element is the payload data.
+If this entry is None, check results in the response object for errors.
+
+### get_devices()
+Get all wifi-switch outlet devices from Vesync api.
+
+Arguments: none
+Returns: ( payload, requests.response )
+
+### turn_off(id)
+Switch an outlet to OFF.
+
+Arguments: the device ID
+Returns: ( payload, requests.response )
+
+### turn_on(id)
+Switch an outlet to ON.
+
+Arguments: the device ID
+Returns: ( payload, requests.response )
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert(path.join(here, 'README.md'), 'rst')
+except(IOError, ImportError):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()\
+
+setup(
+    name='vesync-outlet',
+    version='0.1.0',
+    description='vesync-outlet is a library to control Vesync wifi outlets',
+    long_description=long_description,
+    url='https://github.com/jeffleary00/vesync-outlet',
+    author='Jeff Leary',
+    author_email='sillymonkeysoftware@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+    ],
+    keywords=['vesync', 'iot'],
+    packages=find_packages('vesync_outlet'),
+    package_dir={'': 'vesync_outlet'},
+    zip_safe=False,
+    install_requires=['requests>=2.6.0'],
+)
